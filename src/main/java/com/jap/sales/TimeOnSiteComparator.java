7dadfcc6296a23213faf7710779e1306b99b4aa0
Nilufa Khan <khannilufa5@gmail.com>
2022-09-04 10:53:48 +0530

    TimeOnSiteComparator comparator method implemented

--- src/main/java/com/jap/sales/TimeOnSiteComparator.java
+++ src/main/java/com/jap/sales/TimeOnSiteComparator.java
@@ -6,6 +6,11 @@ import java.util.Comparator;
 public class TimeOnSiteComparator implements Comparator<SalesRecord> {
     @Override
     public int compare(SalesRecord o1, SalesRecord o2) {
-        return 0;
+        if(o1.getTime_on_site() == o2.getTime_on_site()){
+            return 0;
+        }else if(o1.getTime_on_site() < o2.getTime_on_site()){
+            return -1;
+        }
+        return 1;
     }
 }
