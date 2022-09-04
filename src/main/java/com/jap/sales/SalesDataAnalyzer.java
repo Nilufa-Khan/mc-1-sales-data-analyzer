172e7b4249e7c24a0cf2c22cb68aa386d77f1b48
Nilufa Khan <khannilufa5@gmail.com>
2022-09-04 10:45:54 +0530

    Read the data from the file and store in a List

--- src/main/java/com/jap/sales/SalesDataAnalyzer.java
+++ src/main/java/com/jap/sales/SalesDataAnalyzer.java
@@ -1,8 +1,10 @@
 package com.jap.sales;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -12,8 +14,24 @@ import java.util.List;
 public class SalesDataAnalyzer {
     // Read the data from the file and store in a List
    public List<SalesRecord> readFile(String fileName) {
+       List<SalesRecord> salesRecords = new ArrayList<>();
+       FileReader fileReader = null;
+       try {
+           fileReader = new FileReader(fileName);
+           BufferedReader bufferedReader = new BufferedReader(fileReader);
+           String data = "" ;
+           String response = "";
+           while ((data = bufferedReader.readLine()) != null){
+                response = response + data;
+               salesRecords.add(new SalesRecord());
 
-        return null;
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
+
+        return salesRecords;
     }
 
     // Sort the customers based on purchase amount
